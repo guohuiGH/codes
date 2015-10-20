d58531b950402890cf9799c22d7a6844d88ab6dc
guohuiGH <guohui2011915@gmail.com>
2015-10-20 23:53:55 +0800

    algorithm

---
+++ data_structure_algorithm/3_max_subarray.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+    > File Name: 3_max_subarray.cpp
+    > Author: guohui
+    > Mail: guohui1029@foxmail.com 
+    > Created Time: Sun 04 Oct 2015 11:57:54 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main() {
+	int a = {
+	3,3,4,-6,3.-5.6.4.-2.8.1.-2.4.2
+	};
+
+	return 0;
+}
