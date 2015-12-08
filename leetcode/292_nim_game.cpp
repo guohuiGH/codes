0140e25b40c3a568dc01f474d43fba2c11679061
guohuiGH <guohui2011915@gmail.com>
2015-12-08 14:00:02 +0800

    math

---
+++ leetcode/292_nim_game.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: 292_nim_game.cpp
+    > Author: guohui
+    > Mail: guohui1029@foxmail.com 
+    > Created Time: Tue 08 Dec 2015 01:55:41 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+	bool canWinNim (int n) {
+		if (n % 4 != 0)
+			return true;
+	}
+}
